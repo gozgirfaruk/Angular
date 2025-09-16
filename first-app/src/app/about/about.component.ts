ef9987926afea8351fe9ca375685bdc2fae64805
Ömer Faruk Gözegir <gozgirfaruk@gmail.com>
2025-09-16 16:01:00 +0300

    Angular introduction

--- first-app/src/app/about/about.component.ts
+++ first-app/src/app/about/about.component.ts
@@ -1,10 +1,12 @@
 import { Component, ViewEncapsulation } from "@angular/core";
+import { Contact } from "../contact/contact";
 
 @Component({
+    selector:"app-about",
     standalone:true,
     templateUrl:"./about.component.html",
     styleUrl:"./about.component.css",
-    imports:[],
+    imports: [Contact],
     providers:[],
     encapsulation:ViewEncapsulation.None
 
--- first-app/src/app/app.ts
+++ first-app/src/app/app.ts
@@ -1,12 +1,25 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { Contact } from './contact/contact';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,Contact,AboutComponent,NgClass],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('first-app');
+
+  helloWorld:string = "Hello My Name Is `Never Mind`";
+  pclassName:string ="red";
+  clickMe(){
+    alert("Tıklama İşlemi Gerçekleştirildi")
+  };
+
+  changeText(event:any){
+      this.helloWorld = event.target.value;
+  };
 }
